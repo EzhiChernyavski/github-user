a1be8e0a0d92909e4dd142351e3cc93ac15d5018
Ezhi Chernyavski <yurachernyavskiw@gmail.com>
2022-05-23 17:13:17 +0300

    pagination

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loader = () => {
+    return (
+        <div className={style.wrapper}>
+           <div clsaaName={style.}></div>
+        </div>
+    );
+};
+
+export default Loader;
